3b8e17f18f3a517931059be3f2bcdcc318ac1405
BulbaWarrior <vladkalm49@gmail.com>
2020-04-10 20:18:57 +0300

    Basic image generation from chromosome

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ import numpy as np
 import cv2 as cv
 import sys
 
-img = cv.imread("data/1.png")
+upscale_factor=1
+input_img = cv.imread("input.png")
 
-if img is None:
-    sys.exit("Could not read the image")
+def generate_image(chromosome):
+    image = np.zeros((512*upscale_factor,512*upscale_factor,3), np.uint8)
+    # deviding the 512x512 image into an 8X8 grid and filling the grid with the images
+    # according to the chromosome
+    side_size = 512*upscale_factor // 64
+    for i in range(side_size**2):
+        row = i%side_size
+        column = i//side_size
+        imname = "data/" + str(chromosome[i]) + ".png"
+        image[row*64:row*64 + 64, column*64:column*64 + 64] = cv.imread(imname)
+    return image
 
-cv.imshow("Display window", img)
+sample_chromosome = np.random.randint(1, 21000, 64, dtype='uint16')
+res = generate_image(sample_chromosome)
+
+cv.imshow('result', res)
 k = cv.waitKey(0)
+cv.destroyAllWindows()
 
-if k == ord("s"):
-    cv.imwrite("new_image.png", img)
 
